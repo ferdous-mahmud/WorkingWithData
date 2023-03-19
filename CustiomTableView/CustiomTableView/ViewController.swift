05fb0b2bc1297d1dbd81a0c07c51ba43abfc61b4
ferdous-mahmud <ferdous.webdev@gmail.com>
2023-03-19 11:42:55 +0600

    CustomTableView cells added

---
+++ CustiomTableView/CustiomTableView/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CustiomTableView
+//
+//  Created by Ferdous Mahmud Akash on 3/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var cell: UITableViewCell!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
