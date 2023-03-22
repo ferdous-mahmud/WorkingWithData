3f9cddb48527d21abb37220744714b9611fd8d16
ferdous-mahmud <ferdous.webdev@gmail.com>
2023-03-22 17:34:06 +0600

    Splash screen added

---
+++ SplashScreen/SplashScreen/SplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenApp.swift
+//  SplashScreen
+//
+//  Created by Ferdous Mahmud Akash on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
