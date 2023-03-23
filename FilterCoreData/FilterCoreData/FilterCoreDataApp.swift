4b6009afc7ff85f75a47dfe56b9d419409bb3bd6
ferdous-mahmud <ferdous.webdev@gmail.com>
2023-03-23 21:44:43 +0600

    filtering CoreData added

---
+++ FilterCoreData/FilterCoreData/FilterCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilterCoreDataApp.swift
+//  FilterCoreData
+//
+//  Created by Ferdous Mahmud Akash on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct FilterCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
